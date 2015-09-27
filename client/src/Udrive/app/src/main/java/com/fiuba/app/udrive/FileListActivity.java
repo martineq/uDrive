34b641212bb8bc6359db4846f735a512e6206267
e-liva <euge.war@gmail.com>
2015-09-27 00:14:04 -0300

    Move action bar menu to FileListActivity and implement sign out functionality. This closes #42 and closes #43

--- client/src/Udrive/app/src/main/java/com/fiuba/app/udrive/FileListActivity.java
+++ client/src/Udrive/app/src/main/java/com/fiuba/app/udrive/FileListActivity.java
@@ -1,12 +1,17 @@
 package com.fiuba.app.udrive;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.fiuba.app.udrive.model.File;
+import com.fiuba.app.udrive.model.ObjectStream;
 import com.fiuba.app.udrive.model.UserAccount;
 import com.fiuba.app.udrive.network.FilesService;
 import com.fiuba.app.udrive.network.ServiceCallback;
@@ -26,7 +31,6 @@ public class FileListActivity extends AppCompatActivity {
 
     private FilesService mFilesService;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +41,34 @@ public class FileListActivity extends AppCompatActivity {
         this.mFilesAdapter = new FilesArrayAdapter(this, R.layout.file_list_item, this.mFiles);
         ListView list = (ListView)findViewById(R.id.fileListView);
         list.setAdapter(mFilesAdapter);
-        this.mFilesService = new FilesService();
+        this.mFilesService = new FilesService(uAccount.getToken());
         loadFiles();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_file_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Action of signing out
+        if (id == R.id.action_signout) {
+            ObjectStream<UserAccount> os = new ObjectStream<>(MainActivity.getAccountFilename(), FileListActivity.this);
+            os.delete();
+            Intent main = new Intent(FileListActivity.this, MainActivity.class);
+            startActivity(main);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadFiles(){
