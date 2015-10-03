fac94f5b4acdc85c5853f88f63c89dc72ab71373
Martin <martindonofrio@gmail.com>
2015-10-03 19:55:49 -0300

    Agregado de clases para la generacion de tokens

---
+++ server/src/rest/node.h
@@ -0,0 +1,41 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#include "mongoose/mgconnection.h"
+
+
+class Node {
+	public:
+		Node(const char* str);
+		bool handle(MgConnection& conn, const char* url);
+	protected:
+
+		bool match(const char* url) = 0;
+
+		virtual void execute(MgConnection& conn, const char* url) = 0;
+
+		/** Chequea el metodo (METHOD HTTP) de la connecion y ejecuta el metodo correspondiente
+			 */
+		void execute(MgConnection& conn, const char* url);
+
+			/** Metodo que se ejecuta si la connecion es POST
+			 */
+		void executePost(MgConnection& conn, const char* url);
+
+			/** Metodo que se ejecuta si la connecion es GET
+			 */
+		void executeGet(MgConnection& conn, const char* url);
+
+			/** Metodo que se ejecuta si la connecion es DELETE
+			 */
+		void executeDelete(MgConnection& conn, const char* url);
+
+			/** Manejador de metodo no implementado
+			 */
+		void methodNotAllowed(MgConnection& conn, const char* url);
+
+		std::string uri;
+};
+
+
+#endif
---
+++ server/src/rest/token_node.h
@@ -0,0 +1,15 @@
+#ifndef __AUTH_NODE_H__
+#define __AUTH_NODE_H__
+
+#include "../rest/node.h"
+
+class TokenNode : public Node {
+	public:
+		TokenNode();
+
+	protected:
+		void executeGet(MgConnection& conn, const char* url);
+		static std::string CreateToken(const std::string& username);
+};
+
+#endif
