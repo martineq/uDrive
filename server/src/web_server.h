52bdc4e09b022de2680bb2d1c04db115b4c5b8b2
martindonofrio <martindonofrio@gmail.com>
2015-10-04 11:13:17 -0300

    Cambio nombre archivo.

---
+++ server/src/web_server.h
@@ -0,0 +1,38 @@
+#ifndef __WEB_SERVER_H__
+#define __WEB_SERVER_H__
+
+#include <string>
+#include <vector>
+
+using std::string;
+
+#include "mongoose/mongoose.h"
+#include "util/log.h"
+
+extern "C" {
+	#include <pthread.h>
+}
+
+class WEBServer {
+	public:
+
+		WEBServer();
+		~WEBServer();
+
+		const char* setPort(string port);
+		const char* setDocumentRoot(string path);
+		void run();
+		void stop();
+
+
+
+	protected:
+		struct mg_server *server;
+		pthread_t hilo;
+		static void* threadHandler(void*);
+		static int handlerCaller(struct mg_connection*, enum mg_event);
+		int running;
+
+};
+
+#endif
