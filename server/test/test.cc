54e78a276ecfd2e65a47dce439e7c9afc8b8a8cf
Mart <martineq@gmail.com>
2015-10-17 15:27:06 -0300

    Implemento ConfigParser. Agrego test para lectura de archivo ce configuración. #67

--- server/test/test.cc
+++ server/test/test.cc
@@ -1,8 +1,11 @@
 // test.cc
 
 #include <iostream>
-#include "test_basic.h"
+#include <fstream>
+
 #include "gtest/gtest.h"
+#include "test_basic.h"
+#include "../src/config_parser.h"
 
 using namespace std;
 
@@ -17,6 +20,27 @@ TEST(BasicTest, AlwaysOk) {
 
   EXPECT_EQ(1,1);
 }
+
+TEST(YamlTest, ReadsFromFile) {
+  
+  // Create config file
+  ofstream myfile;
+  myfile.open ("config.yml");
+  myfile << "# Configuración de conexión\nip: 127.0.0.1\nport: 8080";
+  myfile.close();
+  
+  // Read config file
+  ConfigParser yp;
+  ConfigParser::stConnection conn;
+  conn.ip = "0";
+  conn.port = "0";
+  yp.load_configuration(conn);
+  
+  EXPECT_EQ(conn.ip,"127.0.0.1");
+  EXPECT_EQ(conn.port,"8080");
+
+}
+
 
 int main(int argc, char **argv) {
 
