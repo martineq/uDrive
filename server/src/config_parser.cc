3961db53a9dd2e0b7a747f75275ec060bb2c678e
Martin <martindonofrio@gmail.com>
2015-10-25 21:09:25 -0300

    Implementacion de la config. del servidor desde config.yml

--- server/src/config_parser.cc
+++ server/src/config_parser.cc
@@ -12,18 +12,19 @@ ConfigParser::~ConfigParser(){
 /**
  * @brief Reads the content of the config file for server. Returns true on success.
  * 
- * @param connection return connection information from file
+ * @param config return config information from file
  * @return bool
  */
-bool ConfigParser::load_configuration(ConfigParser::stConnection& connection){
-
+bool ConfigParser::load_configuration(ConfigParser::Configuration& config){
   // Open file
   std::ifstream config_file(YAML_CONFIG_FILE);
+  
   bool status_ok = true;
 
   // Parsing file
   if(config_file.good()==true){
     try{
+
       // Starts to read YAML file
       YAML::Parser parser(config_file);
       
@@ -32,34 +33,36 @@ bool ConfigParser::load_configuration(ConfigParser::stConnection& connection){
       parser.GetNextDocument(root_node);
       
       // Reads the content
-      load_content(root_node,connection);
+      load_content(root_node,config);
           
     }catch(YAML::Exception& e){
+      std::cout << "Exceotion" << std::endl;
       notify_read_error(__FILE__,__LINE__,e.what(),status_ok);
     }
-
   }else{
+    Log(Log::LogMsgError) << "Problem with config file.";
     notify_read_error(__FILE__,__LINE__,"Error opening config file. ",status_ok);
   }
-
   return status_ok;
 }
 
 
-void ConfigParser::load_content(YAML::Node& root_node, ConfigParser::stConnection& connection){
+void ConfigParser::load_content(YAML::Node& root_node, ConfigParser::Configuration& config){
 
   // Iterate nodes, reading information
   for(YAML::Iterator it=root_node.begin() ; it!=root_node.end() ; ++it){
+
     std::string key;
     key = read_yaml_node_to_string(it.first());
     
-    if( key.compare(YAML_LABEL_IP) == 0 ){ connection.ip = read_yaml_node_to_string(it.second());
-    }else if ( key.compare(YAML_LABEL_PORT) == 0 ){ connection.port = read_yaml_node_to_string(it.second());
-    }else{ /* Future keys */ }
-
-  }
+    if( key.compare(YAML_LABEL_BINDIP) == 0 ){ config.bindip = read_yaml_node_to_string(it.second());
+    }else if ( key.compare(YAML_LABEL_BINDPORT) == 0 ){ config.bindport = read_yaml_node_to_string(it.second());
+    }else if ( key.compare(YAML_LABEL_LOGFILE) == 0 ){ config.logfile = read_yaml_node_to_string(it.second());
+    }else if ( key.compare(YAML_LABEL_LOGLEVEL) == 0 ){ config.loglevel = read_yaml_node_to_string(it.second()); }
 
   return void();
+}
+
 }
 
 
