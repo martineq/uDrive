da833c1c9e38ec97eb6fe6b933e2b00647b1e6bd
Romina <romicorty@gmail.com>
2015-10-26 20:57:48 -0300

    Diseño e implementación de la pantalla de carga de archivos #61

--- client/src/Udrive/app/src/main/java/com/fiuba/app/udrive/network/FilesService.java
+++ client/src/Udrive/app/src/main/java/com/fiuba/app/udrive/network/FilesService.java
@@ -9,13 +9,26 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 import retrofit.http.GET;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
 import retrofit.http.Path;
+import retrofit.mime.TypedFile;
 
 public class FilesService extends AbstractService {
 
     private interface FilesServiceApi {
         @GET("/info/users/{userId}/dir/{dirId}")
         void getFiles(@Path("userId") int userId, @Path("dirId") int dirId, Callback<List<File>> files);
+
+
+        @Multipart
+        @POST("/users/{userId}/dir/{dirId}")
+        void upload(@Path("userId") int userId,
+                    @Path("dirId") int dirId,
+                    @Part("file") TypedFile file,
+                    @Part("fileName") String name,
+                    Callback<List<File>> files);
     }
 
     private FilesServiceApi mFilesServiceApi;
@@ -42,5 +55,32 @@ public class FilesService extends AbstractService {
                 serviceCallback.onFailure(error.getMessage(), status);
             }
         });
+    }
+
+    public void upload(int userId, int dirId,String filePath, final ServiceCallback<List<File>> serviceCallback) {
+        try{
+            java.io.File file = new java.io.File(filePath);
+            TypedFile typedFile = new TypedFile("multipart/form-data", file);
+            String  name = file.getName();
+            mFilesServiceApi.upload(userId,dirId,typedFile,name, new Callback<List<File>>() {
+                @Override
+                public void success(List<File> files, Response response) {
+                    serviceCallback.onSuccess(files, response.getStatus());
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    int status;
+                    if (error.getKind() == RetrofitError.Kind.NETWORK) {
+                        status = 503;
+                    } else
+                        status = error.getResponse().getStatus();
+                    serviceCallback.onFailure(error.getMessage(), status);
+                }
+            });
+        }catch (Exception e){
+            serviceCallback.onFailure("No se pudo cargar el archivo",1);
+        }
+
     }
 }
