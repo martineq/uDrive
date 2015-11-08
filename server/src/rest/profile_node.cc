ea34b19514f41d0ddeeafdf165e6962448531d66
Martin <martindonofrio@gmail.com>
2015-11-08 15:26:20 -0300

    Modificacion del nodo profile que lo integra con la base. Eliminado de hardcodeo. #110

--- server/src/rest/profile_node.cc
+++ server/src/rest/profile_node.cc
@@ -30,7 +30,9 @@ void ProfileNode::executeGet() {
         }
         else{
             std::ostringstream item;
-            /*TODO(martindonofrio): temporary line >>> */ string user_image; getRequestDispatcher()->HARDCODED_get_user_image(userId,user_image,status);
+            char* user_image;
+            std::string size_image;
+            getRequestDispatcher()->get_user_image(userId,user_image,size_image,status);
             Log(Log::LogMsgDebug) << "[" << "printing profile" << "]: firstname: " << user_info.first_name;
             item
             << "{\"firstname\":\""  << user_info.first_name
@@ -40,7 +42,7 @@ void ProfileNode::executeGet() {
             << "\",\"GPSLatitude\":\"" << user_info.gps_lat
             << "\",\"GPSLongitude\":\"" << user_info.gps_lon
             << "\",\"userId\":\"" << userId
-            << "\",\"quotaUseds\":"
+            << "\",\"quotaUseds\":" << user_info.user_quota_used
             << "\",\"quotaTotal\":\"" << user_info.user_quota_total
             << "\",\"quotaUsagePercent\":\"" << user_info.user_quota_used_percentage
             << "\"}";
