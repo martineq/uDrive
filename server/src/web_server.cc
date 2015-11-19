01376157cee1b8ec7e7607b83fe23999700b3a83
Martin <martindonofrio@gmail.com>
2015-11-19 14:43:10 -0300

    Agrego logs en nivel info.

--- server/src/web_server.cc
+++ server/src/web_server.cc
@@ -45,7 +45,8 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
   if (ev == MG_AUTH) {
     return MG_TRUE;   // Authorize all requests
   } else if (ev == MG_REQUEST && !strcmp(conn->uri, "/token")) {
-    TokenNode* tn=new TokenNode(mgConnection);
+      Log(Log::LogMsgInfo) << "[TokenNode]";
+      TokenNode* tn=new TokenNode(mgConnection);
     tn->setRequestDispatcher(RequestDispatcher::get_instance());
       tn->execute();
     return MG_TRUE;
@@ -56,6 +57,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       Log(Log::LogMsgDebug) << "[" << "URI: /info/users" << "], field: " <<field << " Method: "<<conn->request_method <<", Param fileIds: "<<mgConnection.getParameter("fileIds");
 
       if ( ( field == "trash") and (!strncmp(mgConnection.getMethod(),"GET",3)) and  ( lista.size()==5)){
+          Log(Log::LogMsgInfo) << "[InfoTrashNode]";
           InfoTrashNode * itn=new InfoTrashNode(mgConnection);
           itn->setRequestDispatcher(RequestDispatcher::get_instance());
           itn->execute();
@@ -63,6 +65,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "dir") and (!strncmp(mgConnection.getMethod(),"GET",3)) ){
+          Log(Log::LogMsgInfo) << "[InfoNode]";
           InfoNode * in=new InfoNode(mgConnection);
           in->setRequestDispatcher(RequestDispatcher::get_instance());
           in->execute();
@@ -70,7 +73,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "file") and (!strncmp(mgConnection.getMethod(),"GET",3)) and (mgConnection.getParameter("name")!="") ){
-          Log(Log::LogMsgDebug) << "[BÚSQUEDA POR NOMBRE DE ARCHIVO]";
+          Log(Log::LogMsgInfo) << "[SearchFilenameNode]";
           SearchFilenameNode * in=new SearchFilenameNode(mgConnection);
           in->setRequestDispatcher(RequestDispatcher::get_instance());
           in->execute();
@@ -78,7 +81,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "file") and (!strncmp(mgConnection.getMethod(),"GET",3)) and (mgConnection.getParameter("extension")!="") ){
-          Log(Log::LogMsgDebug) << "[BÚSQUEDA POR EXTENSION]";
+          Log(Log::LogMsgInfo) << "[SearchExtensionNode]";
           SearchExtensionNode * in=new SearchExtensionNode(mgConnection);
           in->setRequestDispatcher(RequestDispatcher::get_instance());
           in->execute();
@@ -86,7 +89,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "tags") and (!strncmp(mgConnection.getMethod(),"GET",3)) and (mgConnection.getParameter("tagname")!="") ){
-          Log(Log::LogMsgDebug) << "[BÚSQUEDA POR TAG]";
+          Log(Log::LogMsgInfo) << "[SearchTagNode]";
           SearchTagNode * in=new SearchTagNode(mgConnection);
           in->setRequestDispatcher(RequestDispatcher::get_instance());
           in->execute();
@@ -94,7 +97,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "owners") and (!strncmp(mgConnection.getMethod(),"GET",3)) ){
-          Log(Log::LogMsgDebug) << "[BÚSQUEDA POR PROPIETARIO]";
+          Log(Log::LogMsgInfo) << "[SearchOwnerNode]";
           SearchOwnerNode * in=new SearchOwnerNode(mgConnection);
           in->setRequestDispatcher(RequestDispatcher::get_instance());
           in->execute();
@@ -102,6 +105,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "trash") and (!strncmp(mgConnection.getMethod(),"DELETE",6)) and (mgConnection.getParameter("fileIds")=="") ){
+          Log(Log::LogMsgInfo) << "[DeleteTrashNode]";
           DeleteTrashNode * dtn=new DeleteTrashNode(mgConnection);
           dtn->setRequestDispatcher(RequestDispatcher::get_instance());
           dtn->execute();
@@ -109,6 +113,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "trash") and (!strncmp(mgConnection.getMethod(),"DELETE",6)) ){
+          Log(Log::LogMsgInfo) << "[DeleteFileTrashNode]";
           DeleteFileTrashNode * dftn=new DeleteFileTrashNode(mgConnection);
           dftn->setRequestDispatcher(RequestDispatcher::get_instance());
           dftn->execute();
@@ -116,12 +121,14 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if (( field == "trash") and (!strncmp(mgConnection.getMethod(),"POST",4)) and (mgConnection.getParameter("fileIds")!="") ){
+          Log(Log::LogMsgInfo) << "[RestoreFileTrashNode]";
           RestoreFileTrashNode * rftn=new RestoreFileTrashNode(mgConnection);
           rftn->setRequestDispatcher(RequestDispatcher::get_instance());
           rftn->execute();
           delete rftn;
           return MG_TRUE;
       }else if ( ( field == "trash") and (!strncmp(mgConnection.getMethod(),"POST",4)) and  ( lista.size()>4)){
+          Log(Log::LogMsgInfo) << "[RestoreTrashNode]";
           RestoreTrashNode * rtn=new RestoreTrashNode(mgConnection);
           rtn->setRequestDispatcher(RequestDispatcher::get_instance());
           rtn->execute();
@@ -131,6 +138,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       }else return MG_FALSE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/file/users",11)) {
+    Log(Log::LogMsgInfo) << "[ReceiveFileNode]";
     ReceiveFileNode* rfn=new ReceiveFileNode(mgConnection);
     rfn->setRequestDispatcher(RequestDispatcher::get_instance());
      rfn->execute();
@@ -145,12 +153,14 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       Log(Log::LogMsgDebug) << "[" << "URI: /users" << "], field: " <<field <<" Method: "<<conn->request_method;
 
       if ( ( field == "file") and (!strncmp(mgConnection.getMethod(),"DELETE",6)) ){
+          Log(Log::LogMsgInfo) << "[DeleteFileNode]";
           DeleteFileNode* dfn=new DeleteFileNode(mgConnection);
           dfn->setRequestDispatcher(RequestDispatcher::get_instance());
           dfn->execute();
           delete dfn;
           return MG_TRUE;
       }else if ( ( field == "dir") and (!strncmp(mgConnection.getMethod(),"DELETE",6)) ){
+          Log(Log::LogMsgInfo) << "[DeleteDirNode]";
           DeleteDirNode* ddn=new DeleteDirNode(mgConnection);
           ddn->setRequestDispatcher(RequestDispatcher::get_instance());
           ddn->execute();
@@ -158,6 +168,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "dir") and (!strncmp(mgConnection.getMethod(),"POST",4)) ){
+          Log(Log::LogMsgInfo) << "[CreateDirNode]";
           CreateDirNode* cdn=new CreateDirNode(mgConnection);
           cdn->setRequestDispatcher(RequestDispatcher::get_instance());
           cdn->execute();
@@ -165,6 +176,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "file") and (!strncmp(mgConnection.getMethod(),"GET",3)) ){
+          Log(Log::LogMsgInfo) << "[SendFileNode]";
           SendFileNode* sfn=new SendFileNode(mgConnection);
           sfn->setRequestDispatcher(RequestDispatcher::get_instance());
           sfn->execute();
@@ -172,6 +184,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if ( ( field == "dir") and (!strncmp(mgConnection.getMethod(),"GET",3)) ){
+          Log(Log::LogMsgInfo) << "[SendDirNode]";
           SendDirNode* sfn=new SendDirNode(mgConnection);
           sfn->setRequestDispatcher(RequestDispatcher::get_instance());
           sfn->execute();
@@ -181,6 +194,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       }else return MG_FALSE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/signup",7)) {
+      Log(Log::LogMsgInfo) << "[SignupNode]";
     SignupNode* sn=new SignupNode(mgConnection);
     sn->setRequestDispatcher(RequestDispatcher::get_instance());
       sn->execute();
@@ -188,6 +202,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
     return MG_TRUE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/profile",8)) {
+      Log(Log::LogMsgInfo) << "[ProfileNode]";
       ProfileNode* sn=new ProfileNode(mgConnection);
       sn->setRequestDispatcher(RequestDispatcher::get_instance());
       sn->execute();
@@ -195,6 +210,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       return MG_TRUE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/photo",6)) {
+      Log(Log::LogMsgInfo) << "[UpdatePhotoNode]";
       UpdatePhotoNode* upn=new UpdatePhotoNode(mgConnection);
       upn->setRequestDispatcher(RequestDispatcher::get_instance());
       upn->execute();
@@ -202,6 +218,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       return MG_TRUE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/userfullname",12)) {
+      Log(Log::LogMsgInfo) << "[UpdateUserFullNameNode]";
       UpdateUserFullNameNode* uun=new UpdateUserFullNameNode(mgConnection);
       uun->setRequestDispatcher(RequestDispatcher::get_instance());
       uun->execute();
@@ -213,6 +230,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       vector<string> lista = WEBServer::split(conn->uri, '/');
 
       if (!strncmp(mgConnection.getMethod(),"GET",3) and (lista.size()!=4)){
+          Log(Log::LogMsgInfo) << "[ListTagsNode]";
           ListTagsNode* ltn=new ListTagsNode(mgConnection);
           ltn->setRequestDispatcher(RequestDispatcher::get_instance());
           ltn->execute();
@@ -220,6 +238,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if (!strncmp(mgConnection.getMethod(),"GET",3) and (lista.size()==4)){
+          Log(Log::LogMsgInfo) << "[ListTagsUserNode]";
           ListTagsUserNode* ltn=new ListTagsUserNode(mgConnection);
           ltn->setRequestDispatcher(RequestDispatcher::get_instance());
           ltn->execute();
@@ -227,6 +246,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if (!strncmp(mgConnection.getMethod(),"PUT",3)){
+          Log(Log::LogMsgInfo) << "[UpdateTagsNode]";
           UpdateTagsNode* uun=new UpdateTagsNode(mgConnection);
           uun->setRequestDispatcher(RequestDispatcher::get_instance());
           uun->execute();
@@ -236,15 +256,16 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/revisions/users/",17)) {
       Log(Log::LogMsgDebug) << "[OBTIENE LISTA DE REVISIONES DE UN ARCHIVO]";
+      Log(Log::LogMsgInfo) << "[NO IMPLEMENTADO]";
+
       return MG_TRUE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/fileInfo/user/",15)) {
 
       Log(Log::LogMsgDebug) << "[/fileInfo/user/]";
 
-      ///fileInfo/user/{userid}/files/{fileid}/collaborators
-
       if (!strncmp(mgConnection.getMethod(),"GET",3)){
+          Log(Log::LogMsgInfo) << "[ListCollaboratorsNode]";
           ListCollaboratorsNode* uun=new ListCollaboratorsNode(mgConnection);
           uun->setRequestDispatcher(RequestDispatcher::get_instance());
           uun->execute();
@@ -252,7 +273,7 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
           return MG_TRUE;
 
       }else if (!strncmp(mgConnection.getMethod(),"POST",4)){
-          Log(Log::LogMsgDebug) << "[ACTUALIZACOLABORADORES]";
+          Log(Log::LogMsgInfo) << "[UpdateCollaboratorsNode]";
           UpdateCollaboratorsNode* uun=new UpdateCollaboratorsNode(mgConnection);
           uun->setRequestDispatcher(RequestDispatcher::get_instance());
           uun->execute();
@@ -261,19 +282,21 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       }else return MG_FALSE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/owners/users/",14)) {
+      Log(Log::LogMsgInfo) << "[ListOwnersNode]";
       ListOwnersNode* uun=new ListOwnersNode(mgConnection);
       uun->setRequestDispatcher(RequestDispatcher::get_instance());
       uun->execute();
       delete uun;
       return MG_TRUE;
   }else if ( (ev == MG_REQUEST && !strncmp(conn->uri, "/user/",6))  and (mgConnection.getParameter("q")!="") ){
+      Log(Log::LogMsgInfo) << "[SearchUsersNode]";
       SearchUsersNode* sfn=new SearchUsersNode(mgConnection);
       sfn->setRequestDispatcher(RequestDispatcher::get_instance());
       sfn->execute();
       delete sfn;
       return MG_TRUE;
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/fileinfo/users/",16)) {
-      Log(Log::LogMsgDebug) << "[FILEINFO]";
+      Log(Log::LogMsgInfo) << "[ListInfoElemNode]";
       ListInfoElemNode* sfn=new ListInfoElemNode(mgConnection);
       sfn->setRequestDispatcher(RequestDispatcher::get_instance());
       sfn->execute();
@@ -281,12 +304,14 @@ int WEBServer::handlerCaller(struct mg_connection *conn, enum mg_event ev){
       return MG_TRUE;
 
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/location/users/",16)) {
+      Log(Log::LogMsgInfo) << "[UpdateLocationNode]";
       UpdateLocationNode* sfn=new UpdateLocationNode(mgConnection);
       sfn->setRequestDispatcher(RequestDispatcher::get_instance());
       sfn->execute();
       delete sfn;
       return MG_TRUE;
   } else if (ev == MG_REQUEST && !strncmp(conn->uri, "/filename/users/",16)) {
+      Log(Log::LogMsgInfo) << "[ModifyNameDirNode]";
       ModifyNameDirNode* sfn=new ModifyNameDirNode(mgConnection);
       sfn->setRequestDispatcher(RequestDispatcher::get_instance());
       sfn->execute();
